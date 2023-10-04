896221facb1aae22477b421438a4fb27ac987f8b
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-10-04 01:04:21 -0300

    test_ListarTodo

--- tests/Feature/TareasControllerTest.php
+++ tests/Feature/TareasControllerTest.php
@@ -5,19 +5,37 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\Tareas;
+use App\Models\Tarea;
+use Illuminate\Support\Facades\Log;
+
 
 class TareasControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
+
+    public function test_ListarTodo()
+    {
+
+        
+        $response = $this->get('http://127.0.0.1:8000/api/tarea');
         $response->assertStatus(200);
+
+        $logData = [
+            'ruta' => 'http://127.0.0.1:8000/api/tarea',
+            'Titulo' => $response->getStatusCode(),
+            'Contenido' => $response->getContent(),
+            'Estado' => $response->getContent(),
+            'Autor' => $response->getContent(),
+            
+           
+        ];
+
+        Log::info('Tareas Lista', $logData);
     }
-}
+
+    }
+
+    
+    
+    
+    
