c9f32ef123e4e22e5cbd45ac14ec9315125e46fa
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-10-03 23:57:34 -0300

    Rutas API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareasController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/tarea', [TareasController::class, 'Insertar']);
+Route::get('/tarea', [TareasController::class, 'Listar']);
+Route::delete('/tarea/{id}', [TareasController::class, 'Eliminar']);
+Route::post('/tarea/{id}', [TareasController::class, 'Actualizar']);
