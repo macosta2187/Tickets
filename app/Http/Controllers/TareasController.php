8e080062e82511e61b4d69464ce96308046e3a4f
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-10-03 23:51:31 -0300

    Insertar

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -10,6 +10,22 @@ class TareasController extends Controller
     
 
 
+    public function Insertar(Request $request)
+    {
+
+        $tareas = new Tarea;
+        $tareas->Titulo = $request->input('nombre');
+        $tareas->Contenido = $request->input('calle');
+        $tareas->Estado = $request->input('numero');
+        $tareas->Autor = $request->input('localidad');        
+        $tareas->save();
+        return response()->json(['message' => 'Tarea creada exitosamente'], 200);
+
+    }
+
+
+
+
 
 
 }
